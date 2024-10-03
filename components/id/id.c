4724a5c5abe77d3abb5508fec19ef5d0fbab25dc
BesturogMestur <birgirsig02@gmail.com>
2024-10-03 08:41:15 +0000

    snoop mode works

---
+++ components/id/id.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+#include "lownet.h"
+#include "serial_io.h"
+
+int ON = 0
+uint8_t FAKE_ID = 0x00;
+
+void setId(uint8_t fakeID){
+  FAKE_ID = fakeID;
+}
+
+unit8_t getID(){
+  if(ON){
+    return FAKE_ID;
+  }
+  else{
+    return lownet_get_device_id;
+  }
+}
+
+void onOff(){
+  if(ON){
+    ON = 0;
+    serial_write_line("fake id off");
+  }
+  else{
+    ON = 1;
+    serial_write_line("fake id on");
+  }
+}
